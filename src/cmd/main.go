bad1f7c2ac3b53e657361c5a4261ac9d31b8d52e
samuelmarcos <samuelmarcos9580@gmail.com>
2025-06-07 14:23:21 -0300

    chore: first setup integration test

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	log "desafio_mercado_livre/src/internal/infra/logger"
 	gin "desafio_mercado_livre/src/internal/infra/router"
 	"desafio_mercado_livre/src/internal/infra/web/webserver"
 )
 
 func main() {
+	log := log.NewLogger()
 	router := gin.NewRouter()
-	webserver := webserver.NewWebServer(router, ":8080")
+	webserver := webserver.NewWebServer(log, router, ":8080")
 	webserver.Start()
 }
