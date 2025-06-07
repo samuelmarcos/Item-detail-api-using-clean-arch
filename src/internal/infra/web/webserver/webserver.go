ad6511294fe7b1015b68dd4da97252c6763d078d
samuelmarcos <samuelmarcos9580@gmail.com>
2025-06-07 14:22:39 -0300

    refactor: gracefull shutdown

--- src/internal/infra/web/webserver/webserver.go
+++ src/internal/infra/web/webserver/webserver.go
@@ -1,7 +1,14 @@
 package webserver
 
 import (
+	"context"
+	"desafio_mercado_livre/src/internal/infra/logger"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,13 +20,15 @@ type Handler struct {
 }
 
 type WebServer struct {
+	Logger        logger.Logger
 	Router        *gin.Engine
 	Handlers      []Handler
 	WebServerPort string
 }
 
-func NewWebServer(router *gin.Engine, port string) *WebServer {
+func NewWebServer(logger logger.Logger, router *gin.Engine, port string) *WebServer {
 	return &WebServer{
+		Logger:        logger,
 		Router:        gin.Default(),
 		Handlers:      []Handler{},
 		WebServerPort: port,
@@ -39,5 +48,32 @@ func (w *WebServer) Start() {
 	for _, handler := range w.Handlers {
 		w.Router.Handle(handler.Method, handler.Path, handler.HandlerFunc)
 	}
-	http.ListenAndServe(":"+w.WebServerPort, w.Router)
+
+	serverLog := fmt.Sprintf("Server listening on port %s", w.WebServerPort)
+	w.Logger.Info(serverLog)
+
+	srv := &http.Server{
+		Addr:    w.WebServerPort,
+		Handler: w.Router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			w.Logger.Error("Erro ao iniciar o servidor", "error", err)
+		}
+	}()
+
+	// Captura sinais de interrupção
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	w.Logger.Info("Desligando o servidor...")
+
+	// Timeout para shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		w.Logger.Error("Erro no shutdown do servidor", "error", err)
+	}
+	w.Logger.Info("Servidor finalizado com sucesso")
 }
